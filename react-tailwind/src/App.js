e1fcd2bf009f2d355fe44c720b94ddfcc6c41e98
Erum Dhukka <erumdhukka531@gmail.com>
2021-08-27 17:58:37 -0500

    sample working

---
+++ react-tailwind/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Cards from './components/Cards';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+        <Cards>
+
+        </Cards>
+        
+    </div>
+  );
+}
+
+export default App;
